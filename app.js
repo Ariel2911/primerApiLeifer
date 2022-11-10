d25a9538947915c28f7e210a02cce5bfb88ac388
Ariel Barrios <ariel29111978@hotmail.com>
2022-11-10 19:32:54 -0300

    actuelizando la configuración inicial

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require('cors');
 const app = express();
@@ -8,7 +9,7 @@ app.get('/', (req, res) => {
   res.send('funciona!')
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`conectado en el puerto ${port}...`)
