0afd5ac53fee2c5ab6304fbc6bb2a6a55cecb895
Ariel Barrios <ariel29111978@hotmail.com>
2022-11-10 19:22:09 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('funciona!')
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`conectado en el puerto ${port}...`)
+})
