1b305e4ab2a925ba84f362988e7cbc140fba265e
Ariel Barrios <ariel29111978@hotmail.com>
2023-03-09 13:06:29 -0300

    agrega validación al post de la ruta tracks

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getItems, getItem, createItems } = require('../controllers/tracks');
 const router = express.Router();
+const { validatorCreateItem } = require("../validators/tracks");
+const { getItems, getItem, createItems } = require('../controllers/tracks');
 
 router.get("/", getItems)
 
 router.get("/:id", getItem);
 
-router.post("/", createItems)
+router.post("/", validatorCreateItem, createItems)
 
 module.exports = router;
