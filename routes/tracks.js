026b3f6ae4a3a6b60a2b01bbec8c8ebe407a3fe3
Ariel Barrios <ariel29111978@hotmail.com>
2023-03-09 15:13:58 -0300

    Prueba de middleware

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { validatorCreateItem } = require("../validators/tracks");
+const customHeader = require("../middleware/customHeader");
 const { getItems, getItem, createItems } = require('../controllers/tracks');
 
 router.get("/", getItems)
 
 router.get("/:id", getItem);
 
-router.post("/", validatorCreateItem, createItems)
+router.post("/", validatorCreateItem, customHeader, createItems)
 
 module.exports = router;
