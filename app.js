d6591ce1aa553343e9da9522cac32f92c8962a36
Ariel Barrios <ariel29111978@hotmail.com>
2023-03-13 12:43:48 -0300

    Crea una conección con Slack para enviar errores mayores a 400

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require('cors');
-const dbConnect = require('./config/mongo')
+const loggerStream =  require("./utils/handleLogger");
+const morgaBody = require("morgan-body");
+const dbConnect = require('./config/mongo');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("storage"))
+app.use(express.static("storage"));
+
+morgaBody(app, {
+  noColors: true,
+  stream: loggerStream,
+  skip: function(req, res){
+
+    return res.statusCode < 400; //omite códigos menores a 400
+  }
+
+});
 
 const port = process.env.PORT || 3000;
 
